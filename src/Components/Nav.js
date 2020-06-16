39671e7083b17976ee92a7248442b5ff4f289ecc
ubryce <bknguyen@ualberta.ca>
2020-06-16 14:45:28 -0600

    basic website w nav bar

---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Logo</h3>
+        <ul>
+            <li>Work</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/Components/Work.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Work() {
+  return (
+    <div>
+      <h1>Work Page</h1>
+    </div>
+  );
+}
+
+export default Work;
