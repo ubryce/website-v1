4fc2ab106ee0324e379ecd8469020409fd99daa0
ubryce <bknguyen@ualberta.ca>
2020-06-17 23:35:18 -0600

    changing names of nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,26 +5,16 @@ import  {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 function Navs() {
   return (
     <nav>
-        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+        <Navbar className="custom-nav-bg" collapseOnSelect expand="sm" variant="dark">
         <Navbar.Brand href="#home">bryce.</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
         </Nav>
         <Nav>
-        <Nav.Link href="#deets">More deets</Nav.Link>
-        <Nav.Link eventKey={2} href="#memes">
-          Dank memes
-        </Nav.Link>
+        <Nav.Link >work.</Nav.Link>
+        <Nav.Link >about.</Nav.Link>
+        <Nav.Link >contact.</Nav.Link>
         </Nav>
         </Navbar.Collapse>
       </Navbar>
