21c8d749f4a06469a45e3e132c2c166bf65b3ea6
ubryce <bknguyen@ualberta.ca>
2020-06-19 15:09:02 -0600

    adding typewriter

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Navs from './Components/Nav';
 import Work from'./Components/Work';
 import About from'./Components/About';
 import Contact from './Components/Contact';
+import Display from './Components/Display';
 
 function App() {
   return (
     <div className="App">
       <Navs/>
+      <Display/>
       <Work/>
       <About/>
       <Contact/>
---
+++ src/Components/Display.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+function Display() {
+  return (
+    <div>
+    <div class="hero">
+          <div class="header">
+              <h1 class="line anim-typewriter">software developer.</h1>
+          </div>
+      </div>
+
+    <div class="scroll-down"></div>
+    </div>
+  );
+}
+
+export default Display;
