a678d28d75debac5105f5b161e3f5073c087783d
ubryce <bknguyen@ualberta.ca>
2020-06-21 17:19:28 -0600

    nav bar show and hide on scroll

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import '../App.css';
 
-function Navs() {
+class Navs extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      show: true,
+      scrollPos: 0
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  handleScroll = () => {
+    // console.log(document.body.getBoundingClientRect());
+    this.setState({
+      scrollPos: document.body.getBoundingClientRect().top,
+      show: document.body.getBoundingClientRect().top > this.state.scrollPos
+    });
+  };
+  render() {
+    console.log(this.state);
   return (
-    <nav>
+    <nav className={this.state.show ? "active" : "hidden"}>
             <input type="checkbox" id="nav" className="hidden"></input>
             <label for="nav" className="nav-btn">
                 <i></i>
@@ -22,6 +45,7 @@ function Navs() {
             </div>
         </nav>
   );
+}
 }
 
 export default Navs;
