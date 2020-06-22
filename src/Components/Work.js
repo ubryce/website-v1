4b78ad20730bb240625396b9ec1007b12df2ce94
ubryce <bknguyen@ualberta.ca>
2020-06-21 19:34:46 -0600

    work component finished

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import '../App.css';
+import './CSS/Work.css';
 
 function Work() {
   return (
     <div>
-      <h1>Work Page</h1>
+      <div className="cursor"></div>
+      <div className="wrapper">
+        <div className="project-list">
+          <div className="project p-1">
+            <div className="project-title"><h1>Moodroid</h1></div>
+            <div className="project-categ">Android App</div>
+            <div className="project-overlay"></div>
+          </div>
+          <div className="project p-2">
+            <div className="project-title"><h1>Blockchain Transactions v2</h1></div>
+            <div className="project-categ">Web App</div>
+            <div className="project-overlay"></div>
+          </div>
+          <div className="project p-3">
+            <div className="project-title"><h1>IDK</h1></div>
+            <div className="project-categ">IDK</div>
+            <div className="project-overlay"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
